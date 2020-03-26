058facbbb07e549e4abc6cbf87fd9d87652a504c
tsubasuchan-eggz <36393198+tsubasuchan-eggz@users.noreply.github.com>
2020-03-26 14:41:06 +0900

    first Commit

---
+++ Assets/Steering.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+    float horizontalSpeed = 1.0f;
+    float verticalSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 1);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0);
+        }
+    }
+}
